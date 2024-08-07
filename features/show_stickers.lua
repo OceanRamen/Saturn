803e27af9211504b99a4303aaae582dee3094db0
nekojoe <148056104+nekojoe@users.noreply.github.com>
2024-08-07 22:05:54 +0100

    fix/back button crashes on stats menu

--- features/show_stickers.lua
+++ features/show_stickers.lua
@@ -3,10 +3,12 @@ local nativefs = require("nativefs")
 
 local card_draw_ref = Card.draw
 function Card:draw(layer)
-    if self.ability and self.ability.set == 'Joker' and S.SETTINGS.modules.preferences.show_stickers.enabled then
-        self.sticker = get_joker_win_sticker(self.config.center)
-    else
-        self.sticker = nil
-  end
-  card_draw_ref(self, layer)
+    if self.ability and self.ability.set == 'Joker' and self.area and self.area.type ~= 'title' then
+        if S.SETTINGS.modules.preferences.show_stickers.enabled then
+            self.sticker = get_joker_win_sticker(self.config.center)
+        else
+            self.sticker = nil
+        end
+    end
+    card_draw_ref(self, layer)
 end
